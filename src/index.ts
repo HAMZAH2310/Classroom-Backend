4f072690d64f430778a230cf4fac245012a5e6fc
Hamzah <hamzahzulkhanbrk@gmail.com>
2026-03-31 11:26:58 +0700

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Classroom API is running' });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
